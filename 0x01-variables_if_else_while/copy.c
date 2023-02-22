e89d15e8fa062d47facb0eafe4b7487a6c4bc6e0
myUsername <you@example.com>
2023-02-22 07:51:18 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/copy.c
+++ 0x01-variables_if_else_while/copy.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand - RAND_MAX / 2;
 
 	if (n > 0)
 	{
@@ -20,6 +20,7 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
+	}
 	else
 	{
 		printf("%d is negative\n", n);
