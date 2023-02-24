84026bdf9d7bbe92e9c84e8c813df082a1255323
myUsername <you@example.com>
2023-02-24 14:55:50 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of ba  * se 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		++i;
+	}
+	return (0);
+}
