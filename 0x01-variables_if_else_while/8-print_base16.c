0a81c43cf9ba7cff4ec64092a45a1d6629a393e4
myUsername <you@example.com>
2023-02-24 16:08:44 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in l * owercas
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char lc;
+
+	for (i = 0; i < 16; i++)
+		putchar((i % 16) + '0');
+
+	for (lc = 'a'; lc <= 'f'; lc++)
+		putchar(lc);
+
+	putchar('\n');
+	return (0);
+}
