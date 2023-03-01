f3d1218647a3ac328250f9b3f4d2b2b873e30816
myUsername <you@example.com>
2023-03-01 06:49:15 -0800

    7-last_digit recommit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a * number.
- * @n: the number
+ * @n:the number
  * Return: last digit
  */
+
 int print_last_digit(int n)
 {
-	int l = n % 10;
+	int last;
 
-	if (l < 0)
-
-		l *= -1;
-
-	_putchar(l + '0');
-
-	return (0);
+	last = n % 10;
+	if (last < 0)
+	{
+	last *= -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
