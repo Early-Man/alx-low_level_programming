7f420621d849b36732e12ab0d69904110517c262
myUsername <you@example.com>
2023-03-03 08:46:03 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * isupper - checks uppercase letter
+ * @x: the number to be checked
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c);
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
