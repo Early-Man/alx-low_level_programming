711a116cd9e07bf666d5421933f74af084cc85c0
myUsername <you@example.com>
2023-03-05 14:15:09 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: always success
+ */
+
+int main(void)
+
+	int j, z = 0;
+
+	while (j < 1024)
+	{
+	if ((j % 3 == 0) || (j % 5 == 0))
+	{
+	z += j;
+	}
+	j++;
+	}
+	_putchar(z);
+	return (0);
+}
