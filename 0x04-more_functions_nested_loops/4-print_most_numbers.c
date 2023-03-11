e6abbb4aea180d427f73e6a23a7d0cee6c636feb
myUsername <you@example.com>
2023-03-11 13:31:31 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9
+ * Description: excludes numbers 0 t0 9
+ * Return: numbers 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++);
+	{
+	if (m == 2 || m == 4)
+		continue;
+	_putchar(m + '0');
+	}
+	_putchar('\n');
+}
