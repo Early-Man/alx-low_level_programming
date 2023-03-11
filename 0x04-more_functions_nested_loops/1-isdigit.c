7e9b3f6599b0ceae79c2764bbe79d3bf6dca1abc
myUsername <you@example.com>
2023-03-11 12:50:42 -0800

    1-isdigit.c twitches

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 
 /**
  * _isdigit - checks for digits 0 to 9.
- * @n: variable to be checked
+ * @c: variable to be checked
  * Return: 1 if n is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (n >= '0' && n <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
