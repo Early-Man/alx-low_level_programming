56513a058108f4372a63bd4786e27b3433177fed
Early-Man <ndebelelindokuhle5@gmail.com>
2024-03-19 11:21:03 -0700

    recommit_5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,28 +6,28 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    int start, end;
-    char temp;
+	int length = 0;
+	int start, end;
+	char temp;
 
-    /* Find the length of the string */
-    while (s[length] != '\0')
-        length++;
+	/* Find the length of the string */
+	while (s[length] != '\0')
+		length++;
 
-    /* Set start and end indexes for swapping */
-    start = 0;
-    end = length - 1;
+	/* Set start and end indexes for swapping */
+	start = 0;
+	end = length - 1;
 
-    /* Swap characters from start and end indexes moving towards the middle */
-    while (start < end)
-    {
-        /* Swap characters */
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+	/* Swap characters from start and end indexes moving towards the middle */
+	while (start < end)
+	{
+		/* Swap characters */
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
 
-        /* Move towards the middle */
-        start++;
-        end--;
-    }
+		/* Move towards the middle */
+		start++;
+		end--;
+	}
 }
